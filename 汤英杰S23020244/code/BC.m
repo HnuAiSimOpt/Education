4a4ba4ace3895a280a84e7013aefb36bad6672bf
3Jett9 <153981204+3Jett9@users.noreply.github.com>
2023-12-27 15:22:24 +0800

    Add files via upload

---
+++ 汤英杰S23020244/code/BC.m
@@ -0,0 +1,17 @@
+function [KK,RR]=BC(Node_info,K,R)
+num=find(Node_info(:,2)<1e-9);
+KK=K;
+RR=R;
+for i=1:size(num,1)
+ r=num(i);
+ KK(2*r-1,:)=0;
+ KK(:,2*r-1)=0;
+ KK(2*r-1,2*r-1)=1;
+ KK(2*r,:)=0;
+ KK(:,2*r)=0;
+ KK(2*r,2*r)=1;
+ RR(2*r-1)=0;
+ RR(2*r)=0;
+end
+end
+
