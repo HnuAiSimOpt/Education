b6f64b7c76d0f99faff131317eb42cdcc03aacaf
Lwg9 <958169875@qq.com>
2023-12-27 19:26:26 +0800

    1

---
+++ 刘文广_B2302S0105_第三次作业/刘文广_B2302S0105_第三次作业/ConsoleApplication1/LinearStatic.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <vector>
+#include <memory>
+#include "Node.h"
+#include "Element.h"
+
+class LinearStatic
+{
+protected:
+	DoubleMatrix Stiffness;
+	DoubleArray force;
+	DoubleArray disp;
+
+	Domain* d;
+public:
+	LinearStatic(Domain* dd) : d(dd){}
+	void assemble();
+	
+};
