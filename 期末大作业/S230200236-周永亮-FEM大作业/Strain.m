e24b49cb8a641cc1ccf242c8682290773f579524
xxx11111 <129157108+xxx11111@users.noreply.github.com>
2024-01-14 10:55:06 +0800

    Add files via upload

---
+++ 期末大作业/S230200236-周永亮-FEM大作业/Strain.m
@@ -0,0 +1,19 @@
+function[B]=Strain(J,nnel,dndr,dnds,dndt)
+%构建单元应变矩阵
+B=zeros(6,24);
+J1=inv(J);
+dndx=zeros(nnel,1);
+dndy=zeros(nnel,1);
+dndz=zeros(nnel,1);
+for i=1:nnel
+    dndx(i)=J1(1,1)*dndr(i)+J1(1,2)*dnds(i)+J1(1,3)*dndt(i);
+    dndy(i)=J1(2,1)*dndr(i)+J1(2,2)*dnds(i)+J1(2,3)*dndt(i);
+    dndz(i)=J1(3,1)*dndr(i)+J1(3,2)*dnds(i)+J1(3,3)*dndt(i);
+    Be=[dndx(i) 0 0
+        0 dndy(i) 0
+        0 0 dndz(i)
+        dndy(i) dndx(i) 0
+        0 dndz(i) dndy(i)
+        dndz(i) 0 dndx(i)];
+    B(:,3*(i-1)+1:3*i)=B(:,3*(i-1)+1:3*i)+Be;
+end
