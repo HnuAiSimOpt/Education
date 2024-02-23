4f5753f493752d82d2088e193a148b1fe416a189
HnuAiSimOpt <118522984+HnuAiSimOpt@users.noreply.github.com>
2024-02-23 15:06:10 +0800

    Delete 苏天宇S230200178 directory

--- 苏天宇S230200178/code/Element_stiffness_matrix.m
+++
@@ -1,9 +0,0 @@
-function [k_global,Length_element,c,s] = Element_stiffness_matrix(E,A,x1,y1,x2,y2)
-%% 根据公式求解一个单元的全局刚度矩阵
-Length_element=((y2-y1)^2+(x2-x1)^2)^0.5;
-c=(x2-x1)/Length_element;
-s=(y2-y1)/Length_element;
-Transformation_matrix=[c^2 c*s -c^2 -s*c;s*c s^2 -s*c -s^2;-c^2 -s*c c^2 s*c;-s*c -s^2 s*c s^2];
-k_element=A*E/Length_element;
-k_global=k_element*Transformation_matrix;
-end
