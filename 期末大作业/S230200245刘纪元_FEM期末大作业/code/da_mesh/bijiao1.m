3937540f3a8f7af907cb2f5ea57e5b991e35d3cc
yuanyyzz <161047934+yuanyyzz@users.noreply.github.com>
2024-02-23 23:24:35 +0800

    期末作业
    期末作业

---
+++ 期末大作业/S230200245刘纪元_FEM期末大作业/code/da_mesh/bijiao1.m
@@ -0,0 +1,43 @@
+clc;
+clear
+%导入有限元软件中的位移矩阵
+uus = load('C:\Users\dell\Desktop\有限元作业\数据\uu.txt');      
+uu=[];
+[m n]=size(uus);
+for i=1:m
+    uu(2*i-1, 1)=uus(i, 2);
+    uu(2*i,1)=uus(i,3);
+end
+%导入matlab计算得到的位移矩阵
+uu_fem= load('C:\Users\dell\Desktop\有限元作业\数据\U_fem.txt');
+%导入有限元软件中节点应力矩阵
+s_node= load('C:\Users\dell\Desktop\有限元作业\数据\s_node.txt');
+%导入matlab计算得到的节点应力矩阵
+stress_node= load('C:\Users\dell\Desktop\有限元作业\数据\stress_node.txt');
+%位移计算结果与软件分析结果的差值
+difference_u=uu_fem-uu;
+uu1=uu;
+%x轴应力两种结果的差值
+difference_sx=stress_node-s_node(:,2:4);
+stress_x=stress_node(:,1);
+s_x=s_node(:,2);
+%合位移误差矩阵
+error_u=[];
+%x轴应力误差矩阵
+error_s=[];
+%软件分析结果与matlab计算结果的比值
+e_u=[];
+e_s=[];
+for j=1:2*m
+    error_u(j,1)=difference_u(j,1)/uu_fem(j,1);
+    e_u(j,1)=uu1(j,1)/uu_fem(j,1);
+    error_u_abs=abs(error_u);
+end
+for i=1:m
+    error_s(i,1)=difference_sx(i,1)/stress_x(i,1);
+    e_s(i,1)=s_x(i,1)/stress_x(i,1);
+    error_s_abs=abs(error_s);
+end
+%计算结果与软件分析结果误差的均值
+mean_u=mean(error_u_abs);
+mean_s=mean(error_s_abs);
---
+++ 期末大作业/S230200245刘纪元_FEM期末大作业/code/da_mesh/hou_processing1.m
@@ -0,0 +1,80 @@
+clc;
+clear
+
+%导入节点坐标
+x0 = load('.\txt\x0.txt');
+%导入abaqus分析得到的节点应力数据
+uus = load('.\txt\uu.txt');
+%导入matlab计算得到的节点应力数据
+yingli = load('.\txt\stress_node.txt');
+x=x0(:,2);
+x=x';
+y=x0(:,3);
+y=y';
+[m n]=size(uus);
+u=uus(:,2);
+v=uus(:,3);
+z=[];
+for i=1:m
+    z=[z sqrt(u(i)*u(i)+v(i)*v(i))];
+end
+
+%%合位移
+%插值
+[X,Y,Z]=griddata(x,y,z,linspace(min(x),max(x),1000)',linspace(min(y),max(y),1000),'cubic')
+%云图（等高线图），去掉黑色等高线
+figure,contourf(X,Y,Z,'LineStyle','none')   
+%保持光滑效果
+shading flat;
+hold on;
+colorbar;
+title('合位移云图');
+%三维曲面
+figure,mesh(X,Y,Z)
+colorbar;
+title('合位移三维云图');
+
+%%x轴应力
+%插值
+[X,Y,Z1]=griddata(x,y,yingli(:,1),linspace(min(x),max(x))',linspace(min(y),max(y)),'cubic');
+%云图（等高线图），去掉黑色等高线
+figure,contourf(X,Y,Z1,'LineStyle','none') 
+%保持光滑效果
+shading flat;
+hold on;
+colorbar;
+title('x轴应力云图');
+%三维曲面
+figure,mesh(X,Y,Z1)
+colorbar;
+title('x轴应力三维云图');
+
+%%y轴应力
+%插值
+[X,Y,Z2]=griddata(x,y,yingli(:,2),linspace(min(x),max(x))',linspace(min(y),max(y)),'cubic');
+%云图（等高线图），去掉黑色等高线
+figure,contourf(X,Y,Z2,'LineStyle','none')   
+%保持光滑效果
+shading flat;
+hold on;
+colorbar;
+title('y轴应力云图');
+%三维曲面
+figure,mesh(X,Y,Z2)
+colorbar;
+title('y轴应力三维云图');
+
+%%切应力
+%插值
+[X,Y,Z3]=griddata(x,y,yingli(:,3),linspace(min(x),max(x))',linspace(min(y),max(y)),'cubic');
+%云图（等高线图），去掉黑色等高线
+figure,contourf(X,Y,Z3,'LineStyle','none')   
+%保持光滑效果
+shading flat;
+hold on;
+colorbar;
+title('切应力云图');
+%三维曲面
+figure,mesh(X,Y,Z3)
+colorbar;
+title('切应力三维云图');
