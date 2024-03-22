ce1ecbf84559f839bc878c992c796c9f9911fe85
ISZL2024 <156576030+ISZL2024@users.noreply.github.com>
2024-03-22 23:00:55 +0800

    Add files via upload

---
+++ shengzhaoli202104060419.c
@@ -0,0 +1,38 @@
+
+//盛赵立 202104060419
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define N 10
+double lagrange(double x[],double y[],int n,double t);
+int main()
+{
+    double x[N]={0,1,2,3,4,5,6,7,8,9};
+    double y[N]={0,1,4,9,16,25,36,49,64,81};
+    double t;
+    scanf("%lf",&t);
+    printf("%lf\n",lagrange(x,y,N,t));
+    return 0;
+}
+double lagrange(double x[],double y[],int n,double t)
+{
+    double s=0;
+    for(int i=0;i<n;i++)
+    {
+        double p=1;
+        for(int j=0;j<n;j++)
+        {
+            if(j!=i)
+            {
+                p*=(t-x[j])/(x[i]-x[j]);
+            }
+        }
+        s+=y[i]*p;
+    }
+    return s;
+}
+
+
+
+
+
