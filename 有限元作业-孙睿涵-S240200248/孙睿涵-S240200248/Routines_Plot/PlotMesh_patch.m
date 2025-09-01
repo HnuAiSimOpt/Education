263be048610a0136888b05b1a23c04d55817a8e6
HnuAiSimOpt <118522984+HnuAiSimOpt@users.noreply.github.com>
2025-09-01 18:02:39 +0800

    Revert "第一次提交第三次作业"

--- 有限元作业-孙睿涵-S240200248/孙睿涵-S240200248/Routines_Plot/PlotMesh_patch.m
+++
@@ -1,9 +0,0 @@
-function PlotMesh_patch(elems)
-
-global mNdCrd
-global mLNodS
-
-for i = elems(:)'
-    patch(mNdCrd(mLNodS(i,:),1),mNdCrd(mLNodS(i,:),2),...
-        [0,0.75,0.75],'facealpha',0.5,'edgecolor','b');
-end
